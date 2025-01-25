10d6ad66edd30d0070ae7e39c1bba26239197a0b
leehaim8 <leehaim811@gmail.com>
2025-01-25 15:21:56 +0200

    Updating the app with the home page path and creating the home page

--- ex3/src/components/App.jsx
+++ ex3/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
 
 function App() {
     return (
-        <>
-        hello
-        </>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/HomePage" element={<HomePage />} />
+        </Routes>
     )
 }
 
