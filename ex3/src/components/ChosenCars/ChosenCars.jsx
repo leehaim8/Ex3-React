245831beec3986460d583bd316f5f36f3c920607
leehaim8 <leehaim811@gmail.com>
2025-02-03 20:34:33 +0200

    Adding a component that will display the cars

---
+++ ex3/src/components/ChosenCars/ChosenCars.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CarCard from '../CarCard/CarCard';
+import './ChosenCars.css';
+
+function ChosenCars(props) {
+    return (
+        <div className="car_container">
+            <div className="car_container_title">
+                <h3>Cars Catalogue</h3>
+                <p>{props.cars.length}</p>
+            </div>
+            <div className="car_card">
+                {props.cars.map((car, index) => (
+                    <CarCard
+                        key={index}
+                        name={car.name}
+                        type={car.type}
+                        image={car.image}
+                        fuelCapacity={car.fuelCapacity}
+                        transmission={car.transmission}
+                        capacity={car.capacity}
+                        pricePerDay={car.pricePerDay}
+                        isFavorite={props.favoriteCars.includes(car.name)}
+                        onFavoriteClick={() => props.onFavoriteClick(car.name)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ChosenCars;
